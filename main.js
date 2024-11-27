ee2a54be7bc6ecfb85e4fc1847e78d3f1ab6c399
Kyaku23 <kyaku23desu@gmail.com>
2024-11-27 20:06:34 +0700

    Change the javascript code due to suspicious problem
    I deleted all the code because Im continuing to fix this error as possible.

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-const ImageBox = document.querySelector.selector('.slider-images');
-const Images = document.querySelectorAll('.slider-images img');
-const TotalImages = images.length;
-let Index = 0;
-const SlideInterval = 5000;
 
-function SlideImage() {
-    Index = (index + 1) % TotalImages;
-
-    const OffsetX = -Index * 100;
-
-    ImagesContainer.style.trasnform = `translateX(${offsetX}%)`;
-}
-
-setInterval(SlideImage, SlideInterval);
