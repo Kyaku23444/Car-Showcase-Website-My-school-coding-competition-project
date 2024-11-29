7a0ddb0e57a04a02d53f478e2300975fad1d7a40
Kyaku23 <kyaku23desu@gmail.com>
2024-11-29 13:38:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+const Slides = document.querySelectorAll(".slide-page img");
+let index = 0;
+let InterValid = null;
 
+document.addEventListener("DOMContentLoaded", InitializeSlider);
+
+function InitializeSlider() {
+    if(Slides.length > 0) {
+        Slides[index].classList.add("DisplaySlide");
+        InterValid = setInterval(NextSlide, 5000);
+    }
+}
+
+function ShowSlide(index) {
+    if(index >= Slides.length) {
+        index = 0;
+    } else if(index < 0) {
+        index = Slides.length - 1;
+    }
+
+    Slides.forEach(slide => {
+        slide.classList.remove("DisplaySlide");
+    });
+    Slides[index].classList.add("DisplaySlide");
+}
+
+function PreviousSlide() {
+    index--;
+    ShowSlide(index);
+}
+
+function NextSlide() {
+    index++;
+    ShowSlide(index);
+}
